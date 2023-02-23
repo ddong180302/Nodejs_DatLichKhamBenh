2af1fe8448e3b4a37a3b6776098ca53ebdb637e5
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-02-23 21:21:41 +0700

    commit api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,24 @@
-let handleLogin = (req, res) => {
+import userService from "../services/userService";
+
+let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing inputs parameter!'
+        })
+    }
 
+    let userData = await userService.handleUserLogin(email, password);
+    //check email exits
+    //compare password
+    //return userInfor
+    //access_token: TWT json web token
     return res.status(200).json({
-        message: 'hello world',
-        yourEmail: email
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        user: userData.user ? userData.user : []
     })
 }
 
