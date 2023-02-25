3d46259ca89ca2ea008dbc31799243e84e8f8456
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-02-25 15:44:47 +0700

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,6 +22,27 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id;
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required paramater',
+            users: []
+        })
+    }
+
+    let users = await userService.getAllUsers(id);
+
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'Ok',
+        users
+    })
+}
+
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 }
