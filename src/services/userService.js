539a295d6a02a2caeae4dc86cbd656ba65dcacfc
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-03-28 21:13:24 +0700

    commit upload image

--- src/services/userService.js
+++ src/services/userService.js
@@ -124,7 +124,8 @@ let createNewUser = (data) => {
                     phonenumber: data.phonenumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -179,6 +180,9 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phonenumber = data.phonenumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
                 await user.save();
                 // await db.User.save({
                 //     firstName: data.firstName,
